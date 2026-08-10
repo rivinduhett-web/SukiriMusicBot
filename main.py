fc785c77e3f28f4cb8b72e4bc4c96a3fa0d4b63b
rivinduhett-web <rivindu.hett@gmail.com>
2026-08-10 16:38:18 +0530

    Update main.py

--- main.py
+++ main.py
@@ -51,4 +51,7 @@ async def main():
     await idle()
 
 if __name__ == "__main__":
-    asyncio.run(main())
+    loop = asyncio.new_event_loop()
+    asyncio.set_event_loop(loop)
+    loop.run_until_complete(main())
+
